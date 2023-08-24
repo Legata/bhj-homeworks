f7aed7d2572832bdffa01879d967c21f24353d61
Legata <legata93@gmail.com>
2023-08-24 21:40:41 +0300

    async-requests_v.1.0

---
+++ async-requests/progressbar/task.js
@@ -0,0 +1,15 @@
+const sendForm = document.getElementById('form');
+const progressBar = document.getElementById('progress');
+
+sendForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const xhr = new XMLHttpRequest();
+    const formData = new FormData(sendForm);
+
+    xhr.upload.addEventListener('progress', (event) => {
+        progressBar.value = event.loaded / event.total;
+    })
+    xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/upload');
+    xhr.send(formData);
+});
